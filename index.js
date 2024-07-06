9ef8b239af9c697b1d669760acaf1a519be51b7f
marilynakinyioyoo <marilyn.oyoo@student.moringaschool.com>
2024-07-06 16:31:45 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name) {
+
+    cats.push("Ralph"); 
+
+};
+
+function destructivelyPrependCat(name) {
+
+    cats.unshift("Bob");
+
+};
+
+function destructivelyRemoveLastCat(name) {
+
+    cats.pop("Garfield");
+
+};
+
+
+function destructivelyRemoveFirstCat(name) {
+
+    cats.shift("Milo");
+
+};
+   
+function appendCat(name) {
+
+   return [...cats, "Broom"];
+};
+
+function prependCat(name) {
+
+    return ["Arnold", ...cats];
+}
+
+function removeLastCat() {
+    cats.slice(-1);
+    return ["Milo", "Otis"];
+}
+
+function removeFirstCat() {
+    cats.slice(1);
+    return ["Otis", "Garfield"];
+}
+
+
+
+
+
+
